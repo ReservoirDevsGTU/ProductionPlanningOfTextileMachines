5eba517ec78333e69bf4ed5b2f6bd0225e10b4d9
mehme2 <126786017+mehme2@users.noreply.github.com>
2024-10-16 23:59:05 +0300

    adding request to database

---
+++ php/addRequest.php
@@ -0,0 +1,47 @@
+<?php
+include 'connect.php';
+
+$conn = connectSQL();
+
+$sql = "INSERT INTO PurchaseRequests(RequestID, RequestDeadline, RequestedBy, CreatedBy, CreationDate, RequestStatus, IsDeleted)
+        OUTPUT INSERTED.RequestID
+        VALUES((SELECT ISNULL(MAX(RequestID)+1,1) FROM PurchaseRequests), ?, ?, ?, GETDATE(), 1, 0)";
+
+$input = json_decode(file_get_contents("php://input"), true);
+
+$stmt = sqlsrv_query($conn, $sql, array(
+    $input["RequestDeadline"],
+    $input["RequestedBy"],
+    $input["CreatedBy"]
+    ));
+
+if($stmt !== false) {
+    $id = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)["RequestID"];
+    sqlsrv_free_stmt($stmt);
+    $sql1 = "INSERT INTO PurchaseRequestInfos(RequestInfoID, RequestID, RequestDescription, ManufacturingUnitID, RequestInfoStatus, IsDeleted)
+        VALUES((SELECT ISNULL(MAX(RequestInfoID)+1,1) FROM PurchaseRequestInfos), ?, ?, ?, 1, 0)";
+    $stmt1 = sqlsrv_query($conn, $sql1, array(
+        $id,
+        $input["RequestDescription"],
+        $input["ManufacturingUnitID"]
+        ));
+    sqlsrv_free_stmt($stmt1);
+    $sql2 = "INSERT INTO PurchaseRequestDetails(RequestDetailID, RequestID, MaterialID, RequestedAmount, OfferedAmount, OrderedAmount, ProvidedAmount, MaterialStatus, IsDeleted)
+            VALUES((SELECT ISNULL(MAX(RequestDetailID)+1,1) FROM PurchaseRequestDetails), ?, ?, ?, ?, ?, ?, 1, 0)";
+    foreach($input["Materials"] as $m) {
+        $stmt2 = sqlsrv_query($conn, $sql2, array($id,
+            $m["MaterialID"],
+            $m["RequestedAmount"],
+            $m["OfferedAmount"],
+            $m["OrderedAmount"],
+            $m["ProvidedAmount"]
+            ));
+        sqlsrv_free_stmt($stmt2);
+    }
+}
+else {
+    die(json_encode(sqlsrv_errors(), true));
+}
+
+sqlsrv_close($conn);
+?>
