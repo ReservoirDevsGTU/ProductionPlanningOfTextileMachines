b396e011060cccced1aa83213162d13c02b70590
mehme2 <126786017+mehme2@users.noreply.github.com>
2024-10-16 20:21:04 +0300

    request listing backend

---
+++ php/requestListing.php
@@ -0,0 +1,38 @@
+<?php
+header('Content-Type: application/json');
+
+include 'connect.php';
+
+$sql = "SELECT
+        PurchaseRequests.RequestID,
+        PurchaseRequests.RequestDeadline,
+        Users.UserName,
+        PurchaseRequestInfos.RequestDescription,
+        PurchaseRequests.RequestStatus
+        FROM PurchaseRequests
+        JOIN PurchaseRequestInfos
+        ON PurchaseRequests.RequestID = PurchaseRequestInfos.RequestID
+        JOIN Users
+        ON PurchaseRequests.RequestedBy = Users.UserID
+        WHERE PurchaseRequests.IsDeleted = 0
+        ";
+$stmt = sqlsrv_query($conn, $sql);
+
+$data = [];
+if ($stmt !== false) {
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        $data[] = array(
+            "RequestID" => $row["RequestID"],
+            "RequestDeadline" => $row["RequestDeadline"]->format("Y-m-d"),
+            "RequestedBy" => $row["UserName"],
+            "RequestDescription" => $row["RequestDescription"],
+            "RequestStatus" => $row["RequestStatus"]
+            );
+    }
+    sqlsrv_free_stmt($stmt);
+}
+
+sqlsrv_close($conn);
+
+echo json_encode($data);
+?>
