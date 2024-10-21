f4170da7895c4aa9f442b355736474897e2e13b3
JPassepartout <mbatuhanaltun3388@gmail.com>
2024-10-21 18:27:45 +0300

    editing a request

---
+++ php/getRequestByID.php
@@ -0,0 +1,44 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+include 'connect.php';
+
+$requestID = $_GET['id'];
+
+    $sql = "SELECT
+            PurchaseRequests.RequestID,
+            PurchaseRequests.RequestDeadline,
+            Users.UserName,
+            PurchaseRequestInfos.RequestDescription,
+            PurchaseRequests.RequestStatus
+            FROM PurchaseRequests
+            JOIN PurchaseRequestInfos
+            ON PurchaseRequests.RequestID = PurchaseRequestInfos.RequestID
+            JOIN Users
+            ON PurchaseRequests.RequestedBy = Users.UserID
+            WHERE PurchaseRequests.IsDeleted = 0
+            AND PurchaseRequests.RequestID = $requestID"; 
+
+    $data = [];
+    
+    $stmt = sqlsrv_query($conn, $sql);
+    
+    if ($stmt !== false) {
+        if ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            $data = array(
+                "RequestID" => $row["RequestID"],
+                "RequestDeadline" => $row["RequestDeadline"]->format("Y-m-d"),
+                "RequestedBy" => $row["UserName"],
+                "RequestDescription" => $row["RequestDescription"],
+                "RequestStatus" => $row["RequestStatus"]
+            );
+        }
+        sqlsrv_free_stmt($stmt);
+    }
+    sqlsrv_close($conn);
+
+    echo json_encode($data);
+
+?>
---
+++ php/getRequestsMaterials.php
@@ -0,0 +1,44 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+include 'connect.php';
+
+$requestID = $_GET['id'];
+
+    $sql = "SELECT
+            PurchaseRequestDetails.RequestDetailID,
+            PurchaseRequestDetails.MaterialID,
+            PurchaseRequestDetails.RequestedAmount,
+            PurchaseRequestDetails.OrderedAmount,
+            PurchaseRequestDetails.OfferedAmount,
+            PurchaseRequestDetails.ProvidedAmount,
+            PurchaseRequestDetails.MaterialStatus
+            FROM PurchaseRequestDetails
+            WHERE PurchaseRequestDetails.IsDeleted = 0
+            AND PurchaseRequestDetails.RequestID = $requestID"; 
+
+    $materials = [];
+    
+    $stmt = sqlsrv_query($conn, $sql);
+
+    if ($stmt !== false) {
+        while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            $materials[] = array(
+                "RequestDetailID"   => $row["RequestDetailID"],
+                "MaterialID"        => $row["MaterialID"],
+                "RequestedAmount"   => $row["RequestedAmount"],
+                "OrderedAmount"     => $row["OrderedAmount"],
+                "OfferedAmount"     => $row["OfferedAmount"],
+                "ProvidedAmount"    => $row["ProvidedAmount"],
+                "MaterialStatus"    => $row["MaterialStatus"]
+            );
+        }
+        sqlsrv_free_stmt($stmt);
+    }
+    sqlsrv_close($conn);
+
+    echo json_encode($materials);
+
+?>
