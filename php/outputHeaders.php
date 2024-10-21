a7d843d57a4cd27ed79e1e15c4fa4faba6d58b38
mehme2 <126786017+mehme2@users.noreply.github.com>
2024-10-21 19:13:16 +0300

    security fix

--- php/outputHeaders.php
+++ php/outputHeaders.php
@@ -1,5 +1,5 @@
 <?php
-//Bismillahirrahmanirrahim
+//بِسْــــمِ اللّٰهِ الرَّحْمَـنِ الرَّحِيـمِ
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *'); 
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
