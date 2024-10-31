a50968eb5bd8248a960073cd8787565e1fd35b52
mehme2 <126786017+mehme2@users.noreply.github.com>
2024-11-01 02:12:46 +0300

    update getRequestById to the new db

--- php/getRequestByID.php
+++ php/getRequestByID.php
@@ -6,15 +6,15 @@ $requestID = $_GET['id'];
 
     $sql = "SELECT
             PurchaseRequests.RequestID,
-            PurchaseRequests.RequestDeadline,
+            PurchaseRequestDetails.RequestDeadline,
             Users.UserName,
-            PurchaseRequestInfos.RequestDescription,
+            PurchaseRequestDetails.RequestDescription,
             PurchaseRequests.RequestStatus
             FROM PurchaseRequests
-            JOIN PurchaseRequestInfos
-            ON PurchaseRequests.RequestID = PurchaseRequestInfos.RequestID
+            JOIN PurchaseRequestDetails
+            ON PurchaseRequests.RequestID = PurchaseRequestDetails.RequestID
             JOIN Users
-            ON PurchaseRequests.RequestedBy = Users.UserID
+            ON PurchaseRequestDetails.RequestedBy = Users.UserID
             WHERE PurchaseRequests.IsDeleted = 0
             AND PurchaseRequests.RequestID = $requestID"; 
 
