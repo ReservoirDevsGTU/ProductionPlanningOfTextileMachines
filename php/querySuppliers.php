e79fb8e5fddf2046a71ababb4eb7ff92c3b5f4bb
mehme2 <126786017+mehme2@users.noreply.github.com>
2025-01-12 20:48:14 +0300

    query supplier contact details

--- php/querySuppliers.php
+++ php/querySuppliers.php
@@ -1,6 +1,22 @@
 <?php
 include "query.php";
 
+$contactDetails = array("primary" => "ContactDetailID",
+                        "subTableJoinOn" => array("SupplierID"),
+                        "columns" => array("ContactDetailID" => "scd.ContactDetailID",
+                                           "SupplierID" => "scd.SupplierID",
+                                           "ContactName" => "scd.ContactName",
+                                           "ContactSurname" => "scd.ContactSurname",
+                                           "ContactTitle" => "scd.ContactTitle",
+                                           "ContactPhoneNo" => "scd.ContactPhoneNo",
+                                           "ContactEmail" => "scd.ContactEmail",
+                                          ),
+                        "name" => "SupplierContactDetails scd",
+                        "joins" => "",
+                        "filters" => "scd.IsDeleted = 0",
+                        "subTables" => []
+);
+
 $supplierTable = array("primary" => "SupplierID",
                        "columns" => array("SupplierID" => "s.SupplierID",
                                           "SupplierName" => "s.SupplierName",
@@ -14,7 +30,7 @@ $supplierTable = array("primary" => "SupplierID",
                        "name" => "Suppliers s",
                        "joins" => "",
                        "filters" => "s.IsDeleted = 0",
-                       "subTables" => [],
+                       "subTables" => array("ContactDetails" => $contactDetails),
 );
 
 query($supplierTable);
